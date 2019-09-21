a1e7a0866fdff9e0b255c4e20b71a956229dd08f
ebesso <erikbesso@gmail.com>
2019-09-21 21:58:15 +0200

    Quick fix

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const User = require('../resources/models/user');
 const Case = require('../resources/models/case');
 
-
 router.get('/', function(req, res){
     Case.getCasesWithItems(function(err, cases){
         res.render('index', {cases: cases});
